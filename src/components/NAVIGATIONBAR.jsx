2b9477eb2456cb8329dc14508b7c3193c0128208
IgnacioCourt <101607165+IgnacioCourt@users.noreply.github.com>
2022-10-19 10:35:23 -0300

    Bugs0

--- src/components/NAVIGATIONBAR.jsx
+++ src/components/NAVIGATIONBAR.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 function NavigationBar() {
   return (
     <>
       <Navbar>
-        <h2>Agro<span>Minimarket</span></h2>
+        <h2>
+          Agro<span>Minimarket</span>
+        </h2>
         <div>
           <a href="/">Logo</a>
           <a href="/">Buscar</a>
@@ -13,22 +15,22 @@ function NavigationBar() {
         </div>
       </Navbar>
     </>
-  )
+  );
 }
 
-export default NavigationBar
+export default NavigationBar;
 
 const Navbar = styled.nav`
-  h2{
+  h2 {
     font-weight: 400;
     color: white;
-    span{
+    span {
       font-weight: bold;
     }
-  padding : .4rem ;
-  backround-color: black;
-  display: flex ;
-  align-items: center;
-  justify-content: space-between;
+    padding: 0.4rem;
+    backround-color: black;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
 `;
