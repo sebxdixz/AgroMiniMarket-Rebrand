b866e05cf105d568a4475f011e3a32d64e508706
sebadiaaz <65207775+sebxdixz@users.noreply.github.com>
2022-11-04 20:21:55 -0300

    Listoko poko

--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -1,37 +1,41 @@
 import React from 'react';
-import "./../css/colors.module.css";
+import "./Catalog.css";
+import Catalog from "react-catalog-view";
+
+
+//We conver the data.json in a const data
 
 
 function ProductData(props){
     let products = 
-     [
-       {
-          id: 1,
-          title: "Canvas",
-          description: "High quality canvas shoes.",
-          price: "20",
-          discounted: "15",
-          currency: "$",
-          image: "http://domain.com/images/1.jpg",
-       },
-       {
-          id: 2,
-          title: "Sport shoes",
-          description: "Sporty shoes, durable at affordable ranges.",
-          price: "25",
-          currency: "$",
-          discounted: "15",
-          image: "http://domain.com/images/5.jpg",
-       },
-       {
-          id: 3,
-          title: "Heels",
-          description: "Fashionable trendy heels.",
-          currency: "$",
-          price: "30",
-          image: "http://domain.com/images/6.jpg",
-       }
-    ];
+    [
+        {
+           id: 1,
+           title: "Canvas",
+           description: "High quality canvas shoes.",
+           price: "20",
+           discounted: "15",
+           currency: "$",
+           image: "http://domain.com/images/1.jpg",
+        },
+        {
+           id: 2,
+           title: "Sport shoes",
+           description: "Sporty shoes, durable at affordable ranges.",
+           price: "25",
+           currency: "$",
+           discounted: "15",
+           image: "http://domain.com/images/5.jpg",
+        },
+        {
+           id: 3,
+           title: "Heels",
+           description: "Fashionable trendy heels.",
+           currency: "$",
+           price: "30",
+           image: "http://domain.com/images/6.jpg",
+        }
+     ];
     
     const CONTENT_KEYS = 
     {             
@@ -51,7 +55,7 @@ function ProductData(props){
            contentKeys={CONTENT_KEYS}  
            // JSON Object defining the keys that will be 
            // used from the data array, keys should match. (required)
-           skeleton={0}
+           skeleton={4}
            // Any non zero number will override default cards
            // and will show that many skeleton cards.           
            cardSize="sm"
@@ -59,7 +63,7 @@ function ProductData(props){
            btnOneText="View"
            // Enter text for action button one 
            // or pass empty string to hide.  
-           btnTwoText="Purchase Now"
+           btnTwoText="Compra Ahora"
            // Enter text for action button two 
            // or pass empty string to hide.
            btnOneHandler={(args, event, objectData)=>{
@@ -88,6 +92,8 @@ function ProductData(props){
             // Pass a function which returns JSX to be rendered inside card
             // This function will have 'dataObj' containing JSON of
             // the item each card represents
-        />
-      )
+           }
+           />
+            )
   }
+  export default Catalog;
