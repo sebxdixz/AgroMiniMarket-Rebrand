1f2bdb8deda0c961d718304b39e05020d5359d05
Ignacio Court Palacios <101607165+IgnacioCourt@users.noreply.github.com>
2023-05-15 01:05:59 -0400

    Update inicio.jsx

--- src/paginas/inicio.jsx
+++ src/paginas/inicio.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavigationBar from "../components/navigationBar/NavigationBar";
-import Carrusel from "../components/carrusel/Carrusel";
+import Carrusel from "../components/Carrusel/Carrusel.jsx";
 import { MyDinamicStorage } from "../components/myLocalStorage/MyDinamicStorage";
 import "../css/colors.module.css"; 
 import But from "../components/cards/photos"
